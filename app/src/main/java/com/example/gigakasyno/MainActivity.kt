20562b97d3638d0987b78c579d15cc884e8d1855
Kacper Soczko <kacper.soczko@gmail.com>
2026-03-25 00:03:45 +0100

    Initialize repo

---
+++ app/src/main/java/com/example/gigakasyno/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.gigakasyno
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gigakasyno.ui.theme.GigaKasynoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GigaKasynoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingScreen(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingScreen(name: String, modifier: Modifier = Modifier) {
+    val logo = painterResource(R.drawable.logo)
+    val rouletteImage = painterResource(R.drawable.ruletka)
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Image(painter = rouletteImage, contentDescription = stringResource(R.string.roulette_image))
+        Image(painter = logo, contentDescription = stringResource(R.string.app_logo_description))
+        Button(onClick = {}) {
+            Text(text = stringResource(R.string.greeting_screen_btn_text))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingScreenPreview() {
+    GigaKasynoTheme {
+        GreetingScreen("Android")
+    }
+}
